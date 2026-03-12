e07e14a81b6b657802bd38ad75915b140cf212d4
Chaibi Salah Eddine <ch.s.eddine@gmail.com>
2026-03-12 12:05:16 +0000

    multipage added

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [
         react(),
         keycloakify({
-            accountThemeImplementation: "Multi-Page"
+            accountThemeImplementation: "Multi-Page",
         })
     ]
 });
